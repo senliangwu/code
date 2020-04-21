8ab03d930c9492d98501248fc2d6d831b310cb16
senliangwu <62272510+senliangwu@users.noreply.github.com>
2020-04-21 16:41:53 +0800

    singleton

---
+++ workspace_design23/Design23/src/com/senliang/design/singleton/Singleton01.java
@@ -0,0 +1,29 @@
+package com.senliang.design.singleton;
+
+public class Singleton01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Singleton instance01 = Singleton.getInstance();
+		Singleton instance02 = Singleton.getInstance();
+		System.out.println(instance01==instance02);
+		System.out.println(instance01.getClass().hashCode());
+		System.out.println(instance02.getClass().hashCode());
+
+	}
+
+
+}
+
+//饿汉式（静态常量）
+class Singleton{
+	//构造器私有化，外部不能new
+	private Singleton(){
+	}
+	//静态常量，本类内部创建对象
+	private final static Singleton instance = new Singleton();
+	//提供一个公有的静态方法，返回实例对象
+	public static Singleton getInstance() {
+		return instance;
+	}
+}
